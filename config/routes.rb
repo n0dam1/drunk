a49eada4d207b8f62730a7f22df2fc0eed8757a9
minoru_noda <minoru.noda.0527@gmail.com>
2017-07-20 14:18:05 +0900

    Add login function

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :posts, only: [:index, :new, :create]
   resources :users, only: [:index, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
