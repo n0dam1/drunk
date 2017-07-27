ed248a12119b67d6aa66476ce66a9b71aaf9d61c
minoru_noda <minoru.noda.0527@gmail.com>
2017-07-27 09:37:09 +0900

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,24 @@
-30.times do
-  content = Faker::Lorem.sentence
-  post = Post.new
-  post.content = content
-  post.save
-end
-
+# User
 password = "password"
 User.create(name: "hogehoge", password: password, password_confirmation: password)
 9.times do
   name = Faker::Twitter.user[:screen_name]
   User.create(name: name, password: password, password_confirmation: password)
+end
+
+# Post
+5.times do
+  content = Faker::Lorem.sentence
+  post = Post.new
+  post.content = content
+  post.user_id = User.first.id
+  post.save
+end
+
+25.times do
+  content = Faker::Lorem.sentence
+  post = Post.new
+  post.content = content
+  post.user_id = User.pluck(:id).sample
+  post.save
 end
